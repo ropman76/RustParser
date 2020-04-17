06fcd2364ea07262da9b04c86b0072f21a65a328
Tony Ropson <tropson@skylinetechnologies.com>
2020-04-16 21:06:23 -0500

    first start

---
+++ RustParser/target/debug/RustParser.d
@@ -0,0 +1,1 @@
+C:\Users\tropson\Documents\rustparser\RustParser\RustParser\target\debug\RustParser.exe: C:\Users\tropson\Documents\rustparser\RustParser\RustParser\src\main.rs
---
+++ RustParser/target/debug/deps/RustParser.d
@@ -0,0 +1,5 @@
+C:\Users\tropson\Documents\rustparser\RustParser\RustParser\target\debug\deps\RustParser.exe: src\main.rs
+
+C:\Users\tropson\Documents\rustparser\RustParser\RustParser\target\debug\deps\RustParser.d: src\main.rs
+
+src\main.rs:
