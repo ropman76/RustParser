9284c56f701269d859e384769e29e7ed293f34f0
Tony Ropson <Tony_Ropson@Dell.com>
2023-04-24 10:20:36 -0500

    added input

--- RustParser/src/main.rs
+++ RustParser/src/main.rs
@@ -1,12 +1,24 @@
 
 use std::io;
 use std::io::prelude::*;
-  mod token;
-  mod Lexer;
+mod token;
+mod Lexer;
 
 
 fn main() {
+    println!("Welcome to the Money languauge.");
+    println!("feel free  to typein in commands.");
+    
+    let mut Monkey_input = String::new();
    
+   loop{
+    io::stdin()
+    .read_line(&mut Monkey_input)
+    .expect("Failed to read line");
+
+   }
+  
+
 
     let m =  token::LET;
     let z = token::LET;
